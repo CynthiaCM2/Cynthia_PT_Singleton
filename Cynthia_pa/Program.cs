d7577071a7d878c03adc68d0ef93871532d4476c
cynth <cynth@LAPTOP-FM7HKCC1>
2023-10-27 22:13:09 -0600

    Agregar archivos de proyecto.

---
+++ Cynthia_pa/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton
+{
+    class Program
+    {
+        static void Main(string[] args) 
+        {
+            //Propiedad para que se cree el Singleton//s
+            Console.WriteLine(Singleton.Instance.mensaje);
+            Singleton.Instance.mensaje = "hola marte";
+            Console.WriteLine(Singleton.Instance.mensaje);
+        }
+    }
+}
---
+++ Cynthia_pa/Singleton.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton
+{
+    public class Singleton
+    {   
+        //instancia interna del proyecto//
+        private static Singleton instance = null;
+        public string mensaje = "";
+
+        protected Singleton() {
+            mensaje = "hola mundo";
+        }
+
+        public static Singleton Instance
+        //propiedad //
+        {
+            get
+            //Propiedad para que se regrese la clase instanciada//
+            {
+                if (instance == null)
+                    instance = new Singleton();
+                //Si la propiedad instance es null crea uno nuevo//
+
+                return instance;
+            }
+        }
+    }
+}
